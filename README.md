72f150c099aaf2787e2e1bb1177702c6f0e025cf
gary.yau <gary.yau@dhxmedia.com>
2016-03-16 12:25:45 -0700

    Initial commit of RunCmder ST3 command.

---
+++ README.md
@@ -0,0 +1,11 @@
+# RunCmder
+RunCmder is as a Sublime Text 3 plugin providing commands to directly interact with [Cmder](http://cmder.net/).
+
+## Installation
+1. Open up RunCmder settings from `Preferences > Package Settings > RunCmder > Settings - User`
+2. Add the `cmder_installation_path` setting with the local installation path to Cmder. `Ex: { 'cmder_installation_path': 'C:\\Development\\Cmder\\Cmder.exe' }`
+
+## Commands
+Press `Ctrl+Shift+P` and type one of the following commands
+
+**RunCmder: Run:** Opens Cmder in the view's containing directory.
---
+++ RunCmderCommand.py
@@ -0,0 +1,63 @@
+import os
+import re
+import sublime
+import sublime_plugin
+import subprocess
+
+
+def print_sublime_error(error):
+    sublime.error_message('RunCmder\n%s' % error)
+
+
+class RunCmderBaseCommand(sublime_plugin.TextCommand):
+
+    def get_current_file_directory(self):
+        file = self.view.file_name()
+        directory = os.path.dirname(file)
+        return directory
+
+    def get_setting(self, key):
+        settings = sublime.load_settings('RunCmder.sublime-settings')
+        return settings.get(key)
+
+    def get_cmder_path(self):
+        cmder_path = self.get_setting('cmder_installation_path')
+
+        if not cmder_path:
+            print_sublime_error(
+                'Missing Package Setting: cmder_installation_path'
+            )
+            return
+
+        if not os.path.exists(cmder_path):
+            print_sublime_error('Invalid Path: ' + cmder_path)
+            return
+
+        if os.path.basename(cmder_path) != 'Cmder.exe':
+            print_sublime_error('Cannot find Cmder.exe in ' + cmder_path)
+
+        return cmder_path
+
+    def open_cmder(self, args=None):
+        cmder_path = self.get_cmder_path()
+
+        if not cmder_path:
+            return
+
+        command_args = [
+            cmder_path,
+            '/START',
+            self.get_current_file_directory()
+        ]
+        if args:
+            command_args = command_args + args
+
+        print(command_args)
+
+        subprocess.call(command_args)
+
+
+class RunCmderRun(RunCmderBaseCommand):
+
+    def run(self, edit):
+        self.open_cmder()
